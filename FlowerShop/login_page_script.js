e75e8c8143b43a67b6b44efc0f30734bf46c6bc0
Guilherme <guidumasperes@gmail.com>
2020-09-09 02:02:07 -0300

    Added database and account creation feature

--- FlowerShop/login_page_script.js
+++ FlowerShop/login_page_script.js
@@ -3,21 +3,35 @@ function loginPage() {
 }
 
 function createConfirmation() {
-	var email = document.getElementById("email");
+	var email = document.getElementById("email");				 // Obtain email and password from input
 	var pass = document.getElementById("pass");
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "http://localhost:8080", true);
-	xhttp.setRequestHeader("Content-type", "text/html");
-	xhttp.send(email.value+pass.value);
-	child = document.getElementById("child");
-	var parEmail = document.createElement("p");
-	var parPass = document.createElement("p");
-	var group = document.createElement("div");
-	parEmail.innerHTML = "Your email is: " + email.value
-	parPass.innerHTML = "Your password is: " + pass.value
-	group.appendChild(parEmail);
-	group.appendChild(parPass);
-	var header = document.getElementById("header");
-	child.innerHTML = group.innerHTML;
-	header.innerHTML = "Account created!";
+	var myObj = {e_mail: email.value, password: pass.value};  			// Create json
+	var myJSON = JSON.stringify(myObj);
+	var xhttp = new XMLHttpRequest();             					    // Prepare http request to server
+	xhttp.onreadystatechange = function() {
+    	if(this.readyState === 4 && this.status === 200) {
+    		if(this.responseText === "No") {
+      			child = document.getElementById("child");
+      			var neg = document.createElement("p");
+      			neg.innerHTML = "This account already exists";
+      			child.innerHTML = neg.innerHTML;
+      			var header = document.getElementById("header");
+      			header.innerHTML = "Account not created!";
+    		} else {
+    			child = document.getElementById("child");    // Set variables to do account creation confirmation
+				var parEmail = document.createElement("p");
+				var parPass = document.createElement("p");
+				var group = document.createElement("div");
+				parEmail.innerHTML = "Your email is: " + email.value;
+				parPass.innerHTML = "Your password is: " + pass.value;
+				group.appendChild(parEmail);
+				group.appendChild(parPass);
+				var header = document.getElementById("header");
+				child.innerHTML = group.innerHTML;  				// Confirm the account creation
+				header.innerHTML = "Account created!";
+			}
+		}
+  	};
+	xhttp.open("POST", "http://localhost:8080", false);
+	xhttp.send(myJSON);  											// Send email and pass
 }
--- FlowerShop/server.py
+++ FlowerShop/server.py
@@ -1,9 +1,12 @@
 import http.server
 import socketserver
+import json
+import pymongo
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        print(self.requestline)
         if self.path == "/":
             self.send_header("Content-type", "text/html")
             f = open("index.html", "rb")  # change for index.html later
@@ -20,19 +23,32 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
                 self.send_header("Content-type", "text/javascript")
             path = self.path[1:]
             self.end_headers()
-            f = open(path, "rb")
+            f = open(path, "rb") # We need to read in bytes, to send bytes
             self.wfile.write(f.read())
         f.close()
     def do_POST(self):
+        global db
+        print(self.requestline)
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
         print(post_body)
+        new_user = json.loads(post_body.decode('utf-8')) # As we receive bytes we need to decode
+        print(new_user)
+        col = db["Users"]
+        query = {"email": new_user["e_mail"], "pass": new_user["password"]}
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write("Received POST request with body: ".encode("utf-8") + post_body)
+        if col.count_documents(query) == 1:
+            self.wfile.write("No".encode("utf-8"))
+        else:
+            col.insert_one(query)
+            self.wfile.write("Yes".encode("utf-8"))
 
 def main():
+    global db
+    client = pymongo.MongoClient("mongodb://localhost:27017/")  # Connect do database
+    db = client["FlowerShopDB"]
     PORT = 8080
     Handler = HandleRequests
     httpd = socketserver.TCPServer(("", PORT), Handler)
